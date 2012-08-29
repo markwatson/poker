e6f43b61c47a42d175d9593dd74f5f84cf76efc1
Mark Watson <mw5300@gmail.com>
2012-08-29 16:53:48 -0600

    Fixed some error messages and improved input handling

--- src/poker/core.clj
+++ src/poker/core.clj
@@ -2,6 +2,26 @@
   (:gen-class))
 (use '[clojure.string :as s :only (trim lower-case split)])
 
+;; Strings
+(def invalid-cards-message "
+Error: There were one or more invalid cards. Valid cards are:
+- King of Hearts = Kh
+- Ace of Spades = As
+- 7 of diamonds = 7d
+...
+")
+
+(def hand-length-message "
+Error: A hand must be 5 cards.")
+
+(def intro-message "Enter poker hands followed by [RET] to evaluate.")
+
+
+;; Possible hands
+
+
+
+;; Helper functions
 (defn card-type
   "Given a string such as 'Ah' return the card type such as ('A', 'h')"
   [t]
@@ -15,22 +35,17 @@
   [cards]
   cards)
 
-(def invalid-cards-message "
-There were one or more invalid cards. Valid cards are:
-- King of Hearts = Kh
-- Ace of Spades = As
-- 7 of diamonds = 7d
-...
-")
-
 (defn parse-hand
   "Parses a hand string and returns the set of cards."
   [hand]
   ;; Get a list of the cards
-  (let [cards (map card-type (s/split hand #"\s+"))]
+  (let [cards (map card-type (s/split (s/trim hand) #"\s+"))]
     (cond
      ;; If any cards failed to parse we have a problem.
-     (every? #(not (nil? %)) cards) (println invalid-cards-message)
+     (some nil? cards) invalid-cards-message
+
+     ;; If we don't have a hand
+     (not= (count cards) 5) hand-length-message
 
      ;; Otherwise return the rank
      :else (rank-hand cards))))
@@ -39,11 +54,14 @@ There were one or more invalid cards. Valid cards are:
   "Loops over STDIN and reads commands and poker hands."
   []
   (let [rank (parse-hand (read-line))]
-    (println rank))
+    (println rank)
+    (println))
   (read-hands))
 
+
+;; main
 (defn -main
   "Poker hand loop."
   [& args]
-  (println "Enter a poker hand followed by [RET] to evaluate:")
+  (println intro-message)
   (read-hands))
